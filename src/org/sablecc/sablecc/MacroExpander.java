11840019a1d8e2361f8e3c4652011b730e1d4048
Manuel Geffken <geffken@informatik.uni-freiburg.de>
2012-10-19 14:52:49 +0200

    Reconstruct Paresh's modifications to generate generic visitors with generic parameter and return type

--- src/org/sablecc/sablecc/MacroExpander.java
+++ src/org/sablecc/sablecc/MacroExpander.java
@@ -73,6 +73,11 @@ public class MacroExpander
   {
     List macro = (List) macros.get(macroName);
 
+    if( macro == null ) 
+    {
+    	throw new InternalSableCCError("Could not find macro "+macroName);
+    }
+    
     for(ListIterator li = macro.listIterator(); li.hasNext();)
     {
       if(li.nextIndex() != 0)
